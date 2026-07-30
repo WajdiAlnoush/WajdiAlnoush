4036dc8d2e5923820d31e919317dfb88a1409ccc
Wajdi Alnoush <wajdi.alnoush@gmail.com>
2026-07-30 14:28:47 +0300

    Update update_publications.py

--- scripts/update_publications.py
+++ scripts/update_publications.py
@@ -78,7 +78,7 @@ for paper in latest:
             display_authors.append("*et al.*")
 
         content += ", ".join(display_authors)
-        content += "\n\n"
+        content += "\n"
 
     # Journal + Year
     if paper["journal"]:
